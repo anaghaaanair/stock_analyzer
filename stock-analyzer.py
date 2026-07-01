2c2ce5a61cda2a1464c916d835f93103f3c1a594
Anagha <anaghanair710@gmail.com>
2026-07-01 21:26:38 +0530

    Update stock analyzer

--- stock-analyzer.py
+++ stock-analyzer.py
@@ -1,1 +1,12 @@
-print("Hello, Stock Analyzer!")
+print("==============================================")
+print("Stock Analyzer📈")
+print("==============================================")
+
+ticker=input("Enter ticker: ")
+
+print("You entered: ", ticker)
+print("Loading stock data...")
+
+print()
+print("Stock: ", ticker)
+print("Average return: 0.07%\nVolatility: 2.25%")
